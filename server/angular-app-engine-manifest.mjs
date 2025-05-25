01ef8fdfd9ee8ea950c877df9a81241783edf510
Marwa Nasser <marwa.nasser8133@gmail.com>
2025-05-25 16:56:45 +0300

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://marwa-nasser-mohamed-abdelmatloub.github.io/My-Custom_Portfolio',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
